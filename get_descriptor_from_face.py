495b80bdc98f32c58c0c81712375220077ef0ecb
coneypo <coneypo@gmail.com>
2018-09-07 16:00:38 +0800

    return the face descriptor of face

--- get_descriptor_from_face.py
+++ get_descriptor_from_face.py
@@ -1,5 +1,5 @@
 # 测试
-# 输出单张照片的128D特征face_descriptor
+# 输出单张照片的 128D 特征 face_descriptor
 
 # By        coneypo
 # Blog:     http://www.cnblogs.com/AdaminXie
@@ -11,21 +11,25 @@ from skimage import io
 
 # detector to find the faces
 detector = dlib.get_frontal_face_detector()
+
 # shape predictor to find the face landmarks
 predictor = dlib.shape_predictor("shape_predictor_5_face_landmarks.dat")
+
 # face recognition model, the object maps human faces into 128D vectors
 facerec = dlib.face_recognition_model_v1("dlib_face_recognition_resnet_model_v1.dat")
 
-path_img = "F:/code/python/P_dlib_face_reco/data/get_from_camera/"
+# img to read
+path_img = "data/faces_demo/John_Salley/000197_02159525.jpg"
 
 # 返回单张图像的128D特征
-img = io.imread(path_img+"img_face_2.jpg")
+img = io.imread(path_img)
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 dets = detector(img_gray, 1)
 
 if len(dets) != 0:
     shape = predictor(img_gray, dets[0])
     face_descriptor = facerec.compute_face_descriptor(img_gray, shape)
+    print("The face descriptor of ", path_img)
     print(face_descriptor)
 else:
     print("no face")
