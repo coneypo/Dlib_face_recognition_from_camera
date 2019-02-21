8683a15efc2323466f1a113b74d56ac222eef780
Xie Zhengtian <coneypo@gmail.com>
2019-02-21 22:04:24 +0800

    Update get_faces_from_camera.py

--- get_faces_from_camera.py
+++ get_faces_from_camera.py
@@ -1,4 +1,5 @@
 # 进行人脸录入 / face register
+# 录入多张人脸 / support multi-faces
 
 # Author:   coneypo
 # Blog:     http://www.cnblogs.com/AdaminXie
@@ -21,7 +22,7 @@ detector = dlib.get_frontal_face_detector()
 # Dlib 68 点特征预测器
 predictor = dlib.shape_predictor('data/data_dlib/shape_predictor_68_face_landmarks.dat')
 
-# OpenCv 调用摄像头视频流
+# OpenCv 调用摄像头
 cap = cv2.VideoCapture(0)
 
 # 设置视频参数
@@ -33,35 +34,58 @@ cnt_ss = 0
 # 存储人脸的文件夹
 current_face_dir = 0
 
-# 保存的路径
-path_make_dir = "data/data_faces_from_camera/"
-path_csv = "data/data_csvs_from_camera/"
+# 保存 photos/csv 的路径
+path_photos_from_camera = "data/data_faces_from_camera/"
+path_csv_from_photos = "data/data_csvs_from_camera/"
+
+
+# 新建保存人脸图像文件和数据CSV文件夹
+def pre_work_mkdir():
+
+    # 新建文件夹
+    if os.path.isdir(path_photos_from_camera):
+        pass
+    else:
+        os.mkdir(path_photos_from_camera)
+    if os.path.isdir(path_csv_from_photos):
+        pass
+    else:
+        os.mkdir(path_csv_from_photos)
+
+
+pre_work_mkdir()
 
 
 ##### optional/可选, 默认关闭 #####
 # 删除之前存的人脸数据文件夹
-def pre_clear():
-    folders_rd = os.listdir(path_make_dir)
+def pre_work_deldir():
+    # 删除之前存的人脸数据文件夹
+    # 删除 "/data_faces_from_camera/person_x/"...
+    folders_rd = os.listdir(path_photos_from_camera)
     for i in range(len(folders_rd)):
-        shutil.rmtree(path_make_dir+folders_rd[i])
+        shutil.rmtree(path_photos_from_camera+folders_rd[i])
 
-    csv_rd = os.listdir(path_csv)
+    csv_rd = os.listdir(path_csv_from_photos)
     for i in range(len(csv_rd)):
-        os.remove(path_csv+csv_rd[i])
+        os.remove(path_csv_from_photos+csv_rd[i])
 
-# 每次程序录入之前，删掉之前存的人脸数据
-# pre_clear()
+# 这里在每次程序录入之前, 删掉之前存的人脸数据
+# 如果这里打开，每次进行人脸录入的时候都会删掉之前的人脸图像文件夹
+# pre_work_deldir()
 ##################################
 
 
 # 如果有之前录入的人脸
-# 再之前 person_x 的序号按照 person_x+1 开始录入
+# 在之前 person_x 的序号按照 person_x+1 开始录入
 if os.listdir("data/data_faces_from_camera/"):
     # 获取已录入的最后一个人脸序号
     person_cnt = int(str(os.listdir("data/data_faces_from_camera/")[-1]).split("_")[-1])
 # 如果第一次存储或者没有之前录入的人脸, 按照 person_1 开始录入
 else:
     person_cnt = 0
+
+# 之后用来控制是否保存图像的 flag
+save_flag = 1
 
 while cap.isOpened():
     # 480 height * 640 width
@@ -79,10 +103,10 @@ while cap.isOpened():
     # 按下 'n' 新建存储人脸的文件夹
     if kk == ord('n'):
         person_cnt += 1
-        current_face_dir = path_make_dir + "person_" + str(person_cnt)
+        current_face_dir = path_photos_from_camera + "person_" + str(person_cnt)
         print('\n')
-        for dirs in (os.listdir(path_make_dir)):
-            if current_face_dir == path_make_dir + dirs:
+        for dirs in (os.listdir(path_photos_from_camera)):
+            if current_face_dir == path_photos_from_camera + dirs:
                 shutil.rmtree(current_face_dir)
                 print("删除旧的文件夹:", current_face_dir)
         os.makedirs(current_face_dir)
@@ -91,8 +115,9 @@ while cap.isOpened():
         # 将人脸计数器清零
         cnt_ss = 0
 
+    # 检测到人脸
     if len(faces) != 0:
-        # 检测到人脸
+        # 矩形框
         for k, d in enumerate(faces):
             # 计算矩形大小
             # (x,y), (宽度width, 高度height)
@@ -108,11 +133,13 @@ while cap.isOpened():
 
             # 设置颜色 / The color of rectangle of faces detected
             color_rectangle = (255, 255, 255)
-            if (d.right()+ww) > 640 or (d.bottom()+hh>480) or (d.left()-ww < 0) or ( d.top()-hh < 0):
+            if (d.right()+ww) > 640 or (d.bottom()+hh > 480) or (d.left()-ww < 0) or (d.top()-hh < 0):
                 cv2.putText(img_rd, "OUT OF RANGE", (20, 300), font, 0.8, (0, 0, 255), 1, cv2.LINE_AA)
                 color_rectangle = (0, 0, 255)
+                save_flag = 0
             else:
                 color_rectangle = (255, 255, 255)
+                save_flag = 1
 
             cv2.rectangle(img_rd,
                           tuple([d.left() - ww, d.top() - hh]),
@@ -122,14 +149,15 @@ while cap.isOpened():
             # 根据人脸大小生成空的图像
             im_blank = np.zeros((int(height*2), width*2, 3), np.uint8)
 
-            # 按下 's' 保存摄像头中的人脸到本地
-            if kk == ord('s'):
-                cnt_ss += 1
-                for ii in range(height*2):
-                    for jj in range(width*2):
-                        im_blank[ii][jj] = img_rd[d.top()-hh + ii][d.left()-ww + jj]
-                cv2.imwrite(current_face_dir + "/img_face_" + str(cnt_ss) + ".jpg", im_blank)
-                print("写入本地：", str(current_face_dir) + "/img_face_" + str(cnt_ss) + ".jpg")
+            if save_flag:
+                # 按下 's' 保存摄像头中的人脸到本地
+                if kk == ord('s'):
+                    cnt_ss += 1
+                    for ii in range(height*2):
+                        for jj in range(width*2):
+                            im_blank[ii][jj] = img_rd[d.top()-hh + ii][d.left()-ww + jj]
+                    cv2.imwrite(current_face_dir + "/img_face_" + str(cnt_ss) + ".jpg", im_blank)
+                    print("写入本地：", str(current_face_dir) + "/img_face_" + str(cnt_ss) + ".jpg")
 
     # 显示人脸数
     cv2.putText(img_rd, "Faces: " + str(len(faces)), (20, 100), font, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
@@ -137,7 +165,7 @@ while cap.isOpened():
     # 添加说明
     cv2.putText(img_rd, "Face Register", (20, 40), font, 1, (0, 0, 0), 1, cv2.LINE_AA)
     cv2.putText(img_rd, "N: New face folder", (20, 350), font, 0.8, (0, 0, 0), 1, cv2.LINE_AA)
-    cv2.putText(img_rd, "S: Save face", (20, 400), font, 0.8, (0, 0, 0), 1, cv2.LINE_AA)
+    cv2.putText(img_rd, "S: Save current face", (20, 400), font, 0.8, (0, 0, 0), 1, cv2.LINE_AA)
     cv2.putText(img_rd, "Q: Quit", (20, 450), font, 0.8, (0, 0, 0), 1, cv2.LINE_AA)
 
     # 按下 'q' 键退出
