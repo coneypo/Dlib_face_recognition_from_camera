3313d914141d95c40367bab904f992cf2a39d0f8
Xie, Zhengtian <zhengtian.xie@intel.com>
2020-07-03 13:56:05 +0800

    push from gitlab test

--- how_to_use_camera.py
+++ how_to_use_camera.py
@@ -31,7 +31,7 @@ cap = cv2.VideoCapture(0)
 18. cv2.CAP_PROP_RECTIFICATION Rectification flag for stereo cameras (note: only supported by DC1394 v 2.x backend currently)
 """
 
-# The default shape of camera will be 640x480 in Windows or Ubuntu
+# The default size of frame from camera will be 640x480 in Windows or Ubuntu
 # So we will not set "cap.set" here, it doesn't work
 # cap.set(propId=cv2.CAP_PROP_FRAME_WIDTH, value=cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 
